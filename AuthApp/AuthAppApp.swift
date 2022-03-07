9c7ba6db709d16670c30739641f6b3c6ec328346
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-07 06:00:58 +0530

    Initial Commit

---
+++ AuthApp/AuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthAppApp.swift
+//  AuthApp
+//
+//  Created by shehan karunarathna on 2022-03-07.
+//
+
+import SwiftUI
+
+@main
+struct AuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
